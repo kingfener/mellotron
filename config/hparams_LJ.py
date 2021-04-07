19aa7c423688c48bda0d773a6f6aa3f1305ccdc5
king <kingfener@gmail.com>
2021-04-07 23:45:55 +0800

    up

--- config/hparams_LJ.py
+++ config/hparams_LJ.py
@@ -1,7 +1,29 @@
 import tensorflow as tf
-# from text.symbols import symbols
+import sys,os
+# sys.path.append('/media/U1T/SProject/nvidia/mellotron')
+# sys.path.insert(0, os.path.abspath('../mellotron'))   # ok
+# sys.path.insert(0, os.path.abspath(__file__))  
+sys.path.insert(0, os.path.realpath(''))  # ok， 
 
-symbols=['']
+# pwd=/media/U1T/SProject/nvidia/mellotron
+# ├── config
+# │   ├── hparams_LJ.py
+# │   ├── hparams.py
+# │   └── __pycache__
+# │       ├── hparams_LJ.cpython-37.pyc
+# │       └── hparams_LJ.cpython-38.pyc
+# ├── text
+# │   ├── cleaners.py
+# │   ├── cmudict.py
+# │   ├── __init__.py
+# │   ├── LICENSE
+# │   ├── numbers.py
+# │   └── symbols.py
+
+
+print('sys.path=',sys.path)
+from text.symbols import symbols
+
 def create_hparams(hparams_string=None, verbose=False):
     """Create model hyperparameters. Parse nondefault from given string."""
 
